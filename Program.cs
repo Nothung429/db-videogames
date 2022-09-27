7f559832b0b73346c6d7c88cbf7a7ad210e109d0
Nothung429 <frank.gatto96.fg@gmail.com>
2022-09-27 15:48:10 +0200

    add car chart

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿//DIAGRAMMA VIDEOGAME
 //Creare lo schema e-r con tutte le entità necessarie a gestire le informazioni di un archivio di E-sports.
 //    1- Ciascun videogame ha un nome, una trama, una data di rilascio ed è prodotto da una software house, la quale, a sua volta, è identificata da un nome, una partita iva, città e nazione.
 //    2- I videogames possono essere disponibili su diverse piattaforme (PlayStation 4, PlayStation 5, XBox One, XBox Serie X, Windows, Nintendo Switch, Google Stadia, ...) e hanno diverse categorie (avventura, strategia, RPG, sparatutto, calcio, ...) e classificazioni PEGI (PEGI 7, PEGI 12, PEGI 18, Violenza, Paura, Gioco d’azzardo, ...)
@@ -9,3 +7,12 @@ Console.WriteLine("Hello, World!");
 //    5- I giocatori, al momento dell’iscrizione, devono fornire nome, cognome, nickname di gioco, e città di provenienza.
 //    6- I videogames possono essere recensiti dai giocatori, i quali oltre al titolo e al testo, possono valutare i videogame con un rating da 1 a 5.
 //    7- Ogni anno vengono votati i migliori videogiochi, che possono così vincere diversi premi (gioco dell’anno, miglior narrativa, miglior colonna sonora, miglior gioco indipendente, gioco più atteso, ...)
+
+//TABELLA AUTOMOBILI
+//per chi ha terminato, proseguiamo l’esercitazione con un esercizio differente.
+//Concentriamoci ora sui dati di una tabella, che tipi di colonne dovremmo associare ad ogni campo?
+//Ma sopratutto, quali campi decidiamo di rappresentare?
+//Provate a modellizzare la struttura di una tabella per memorizzare tutti i dati riguardanti delle auto usate messe in vendita da un concessionario
+//Esatto, andiamo a fantasia, ma non troppo!
+//Cercate di immaginare ciò che realmente andrebbe progettato.
+//Mi raccomando, una sola tabella, quella dell’automobile!
